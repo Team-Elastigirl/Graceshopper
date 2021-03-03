8010451335dbcc10e85c8ba35e689833c4e95fc7
SofijaSutton <srlsutton@gmail.com>
2021-03-03 18:01:58 -0500

    Feat(Components/AllProducts): adds redux to AllProducts, issues connecting

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,25 +1,51 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {fetchProducts} from '../store/products'
 
 /**
  * COMPONENT
  */
 export class AllProducts extends React.Component {
+  // constructor(props) {
+  //     super(props);
+  // }
+
+  componentDidMount() {
+    console.log(this.props)
+    this.props.getProducts()
+  }
+
   render() {
+    const {products} = this.props
+    console.log(this.props, 'red')
+    // let productsArray = this.props.products
+    // console.log(this.props, productsArray);
+
     return (
       <div>
-        <h1>All Products</h1>
+        <h1>Constellation Getaways</h1>
         <div className="single-product">
           {/* <Link to={`/products/${product.id}`}> */}
-          <h3>Product Name Goes Here</h3>
-          <h3>Product Name Goes Here</h3>
-          <h3>Product Name Goes Here</h3>
-          <h3>Product Name Goes Here</h3>
-          <h3>Product Name Goes Here</h3>
-          <h3>Product Name Goes Here</h3>
+          <h3 />
         </div>
       </div>
     )
   }
 }
+
+const mapState = state => {
+  console.log('inside mapState')
+  return {
+    products: state.products
+  }
+}
+
+const mapDispatch = dispatch => {
+  console.log('inside mapDispatch - purple')
+  return {
+    getProducts: () => dispatch(fetchProducts())
+  }
+}
+
+export default connect(mapState, mapDispatch)(AllProducts)
--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,13 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import productsReducer from './products'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  products: productsReducer,
+  user
+})
+//add subreducers as needed above ie products/singleProduct/singleUser/Guest(maybe?)
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -12,3 +17,4 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './products'
