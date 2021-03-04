ed7a3bb800bcf62450d4ac14fe6e81dd4e2597f6
SofijaSutton <srlsutton@gmail.com>
2021-03-03 21:27:58 -0500

    Feat(Component/AllProducts): creates AllProducts component and adds FE: AllProducts route to index.js

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -7,27 +7,43 @@ import {fetchProducts} from '../store/products'
  * COMPONENT
  */
 export class AllProducts extends React.Component {
-  // constructor(props) {
-  //     super(props);
-  // }
-
   componentDidMount() {
-    console.log(this.props)
+    // console.log(this.props)
     this.props.getProducts()
   }
 
   render() {
-    const {products} = this.props
+    //const {products} = this.props
     console.log(this.props, 'red')
-    // let productsArray = this.props.products
-    // console.log(this.props, productsArray);
+    let productsArray = this.props.products
 
     return (
       <div>
         <h1>Constellation Getaways</h1>
-        <div className="single-product">
-          {/* <Link to={`/products/${product.id}`}> */}
-          <h3 />
+        <div className="products">
+          {productsArray.length ? (
+            productsArray.map(product => (
+              <div className="solo-product" key={productsArray.id}>
+                <Link to={`/products/${product.id}`}>
+                  <h3>{product.name}</h3>
+                  <img
+                    src={product.imageUrl}
+                    allt={product.name}
+                    style={{width: '400px'}}
+                  />
+                  <p>{product.price}</p>
+                  <p>{product.location}</p>
+                  <p>{product.description}</p>
+                  <p>{product.disclaimer}</p>
+                </Link>
+              </div>
+            ))
+          ) : (
+            <h3>
+              Booked Out! No available Constellation Trips at this time, check
+              again soon!
+            </h3>
+          )}
         </div>
       </div>
     )
