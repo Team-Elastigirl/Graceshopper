4395e4e03308fe6680c2e4beec07fe73b60fbb45
SofijaSutton <srlsutton@gmail.com>
2021-03-06 18:52:17 -0500

    Feat(style): adds styling

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -10,9 +10,18 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <h1>Star Hopper</h1>
-          <Link to="/home">Home</Link>
-          <Link to="/products">Products</Link>
+          <a>
+            <img src="rocket.png" alt="rocketlogo" style={{width: '50px'}} />
+          </a>
+          <a>
+            <h1>Star Hopper</h1>
+          </a>
+          <a>
+            <Link to="/home">Home</Link>
+          </a>
+          <a>
+            <Link to="/products">Products</Link>
+          </a>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -20,14 +29,29 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <h1>Star Hopper</h1>
-          <Link to="/products">Products</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+          <a>
+            <img src="rocket.png" alt="rocketlogo" style={{width: '50px'}} />
+          </a>
+          <a>
+            <h1>Star Hopper</h1>
+          </a>
+          <a>
+            <Link to="/products">Products</Link>
+          </a>
+          <a>
+            {' '}
+            <Link to="/login">Login</Link>
+          </a>
+          <a>
+            {' '}
+            <Link to="/signup">Sign Up</Link>
+          </a>
         </div>
       )}
       <div>
-        <Link to="/cart">Cart</Link>
+        <a>
+          <Link to="/cart">Cart</Link>
+        </a>
       </div>
     </nav>
     <hr />
