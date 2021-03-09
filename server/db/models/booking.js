89e309e6096c6542529ab13afc3fdff799a70278
Danny Serrano <danser829@gmail.com>
2021-03-08 22:59:29 -0500

    FIX: add/remove/edit cart

--- server/db/models/booking.js
+++ server/db/models/booking.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Booking = db.define('booking', {
-  quantity: {
+  amount: {
     type: Sequelize.INTEGER,
     allowNull: false,
     validate: {
