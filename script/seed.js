1ad3c7869eb10b61ff927727cbcf317d046c7231
SofijaSutton <srlsutton@gmail.com>
2021-03-09 16:20:35 -0500

    Feat(SEED): adds 3 more products t oseed

--- script/seed.js
+++ script/seed.js
@@ -107,6 +107,42 @@ async function seed() {
       location: '{NQ1 +90°}',
       disclaimer:
         'Star Hopper holds no responsibility or liability for lost property, damage, bodily harm, trips extending beyond planned trip duration up to but not excluding additional 10 years of travel time, or collection of lost properties or bodies.'
+    },
+    {
+      name: 'Cassiopeia, the Seated Queen',
+      quantity: 10,
+      price: 5000,
+      description:
+        'This five-star package is all about supporting women+ (this includes trans and nonbinary folx)! Cassiopeia was historically considered vain, but on this trip we celebrate a powerful woman on the throne and encourage confident learning and exploration. This trip follows the ‘W’-patten of the queen’s throne, making time on each star to explore individual development and community building. From group star climbing on Gamma that pushes us to trust and rely on each other’s experiences, to philosophical debates about the role of mythology and evolving roles of women+ in society while basking in the beautiful star-lights of Delta, each trip is designed to build as sense of wonder in the stars, challenge the views of the past, and leave each group feeling closer and more informed about themselves than when they started. Finally, on the final star of Epsilon Cassiopeiae, we have some fun recreating our own individual space-walk thrones and finish with a calming return-trip tea suited for royalty.',
+      imageUrl:
+        'https://images.unsplash.com/photo-1450849608880-6f787542c88a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1266&q=80',
+      location: '{SQ25 +90°}',
+      disclaimer:
+        'Star Hopper holds no responsibility or liability for lost property, damage, bodily harm, trips extending beyond planned trip duration up to but not excluding additional 10 years of travel time, or collection of lost properties or bodies.'
+    },
+    {
+      name: 'Aquila, the Eagle',
+      quantity: 10,
+      price: 8500,
+      description:
+        'At Star Hopper, Aquila is for lovers. This epic ten-star trip is all about romance and bonding. This constellation, visible with the naked eye back on earth, represents the bird that carried Zeus/Jupiter’s thunderbolts. We hope Aquila can still hold a spark for you and your loved one! Starting off in the Milky Way, this trip warms up with 360° galaxy views and private chambers each with an external window for impressive sights. At Altair, the brightest star, we have fun with our partners and focus on friendship through space-games. On the 15 Aql double star, we send each couple off to a respective orbiting planet for their own, private, camping experience. We’ll pack the picnic! On the final star of Aquilae, we admire the star’s red-colored glow and our partner’s eyes with a paint and sip perfect for creating a memento for your time in space together. ',
+      imageUrl:
+        'https://images.unsplash.com/photo-1614089254151-676cc373b01e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+      location: '{NQ4 -75°}',
+      disclaimer:
+        'Star Hopper holds no responsibility or liability for lost property, damage, bodily harm, trips extending beyond planned trip duration up to but not excluding additional 10 years of travel time, or collection of lost properties or bodies.'
+    },
+    {
+      name: 'Big Dipper, the Plough',
+      quantity: 7,
+      price: 5500,
+      description:
+        'The Big Dipper is a classic. Often the first constellation children learn, this trip celebrates family and science. Starting on the handle of the dipper, we move along learning about space exploration, mythology, navigation, travel and research technology, and artistic representation. Children and their families will see stunning views through a variety of windows, telescopes, and monitor readings on Polaris. They will learn about mythology while putting on a little play of the many stories of Ursa Major (Greater Bear) on Megrez. Parents will enjoy a relaxing day passing Alioth as all the children have a hackathon to create a star-catcher game introducing them to some of our top-notch space-travel technologies. Everyone will enjoy getting their hands dirty on Alkaid on craft day! The craft changes every trip, so prepared to be surprised! At the end of the week, families will have learned a great deal, created many memories together, and have earned a well-deserved return trip full of starry dreams.',
+      imageUrl:
+        'https://images.unsplash.com/photo-1516849677043-ef67c9557e16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+      location: '{SQ4 +55°}',
+      disclaimer:
+        'Star Hopper holds no responsibility or liability for lost property, damage, bodily harm, trips extending beyond planned trip duration up to but not excluding additional 10 years of travel time, or collection of lost properties or bodies.'
     }
   ]
 
